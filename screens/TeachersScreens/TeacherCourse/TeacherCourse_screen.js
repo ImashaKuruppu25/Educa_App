a97a96694d95fa1f3b936d3d33a2cdd439f9841c
Ihill Dewpura <88815545+Sandushan8@users.noreply.github.com>
2022-11-05 16:47:19 +0530

    Teacher profile page complete

--- screens/TeachersScreens/TeacherCourse/TeacherCourse_screen.js
+++ screens/TeachersScreens/TeacherCourse/TeacherCourse_screen.js
@@ -1,6 +1,6 @@
 import { View, Text } from "react-native";
 import React from "react";
-import TeacherNavigation from "./TeacherComponents/Navigation/TeacherNavigation";
+import TeacherNavigation from "../TeacherComponents/Navigation/TeacherNavigation";
 
 const TeacherCourse_screen = () => {
   return (
