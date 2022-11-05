f53a11c18bfed51fa551c925bd2e0ce006586dcd
Ihill Dewpura <88815545+Sandushan8@users.noreply.github.com>
2022-11-05 17:21:03 +0530

    Few changes in teacher general profile

--- screens/TeachersScreens/TeacherProfile/General/General.js
+++ screens/TeachersScreens/TeacherProfile/General/General.js
@@ -5,7 +5,8 @@ import Materialicons from "react-native-vector-icons/MaterialIcons";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import Fontisto from "react-native-vector-icons/Fontisto";
 import Feather from "react-native-vector-icons/Feather";
-import { TextInput } from "react-native-paper";
+import Entypo from "react-native-vector-icons/Entypo";
+import { Button, TextInput } from "react-native-paper";
 
 export const General = () => {
   const [nameEdit, setnameEdit] = useState(false);
@@ -37,13 +38,22 @@ export const General = () => {
               </View>
             </>
           ) : (
-            <View style={{ flex: 13 }}>
-              <TextInput
-                style={{ backgroundColor: "white", height: 40 }}
-                placeholder="Name"
-                onChange={() => setnameEdit(false)}
-              />
-            </View>
+            <>
+              <View style={{ flex: 13 }}>
+                <TextInput
+                  style={{ backgroundColor: "white", height: 40 }}
+                  placeholder="Name"
+                />
+              </View>
+              <View style={{ flex: 1, paddingTop: 15 }}>
+                <Entypo
+                  name="cross"
+                  size={30}
+                  color="red"
+                  onPress={() => setnameEdit(false)}
+                />
+              </View>
+            </>
           )}
         </View>
         <View style={{ flexDirection: "row", marginBottom: 10 }}>
@@ -66,13 +76,22 @@ export const General = () => {
               </View>
             </>
           ) : (
-            <View style={{ flex: 13 }}>
-              <TextInput
-                style={{ backgroundColor: "white", height: 40 }}
-                placeholder="Email"
-                onChange={() => setemailEdit(false)}
-              />
-            </View>
+            <>
+              <View style={{ flex: 13 }}>
+                <TextInput
+                  style={{ backgroundColor: "white", height: 40 }}
+                  placeholder="Email"
+                />
+              </View>
+              <View style={{ flex: 1, paddingTop: 15 }}>
+                <Entypo
+                  name="cross"
+                  size={30}
+                  color="red"
+                  onPress={() => setemailEdit(false)}
+                />
+              </View>
+            </>
           )}
         </View>
         <View style={{ flexDirection: "row", marginBottom: 10 }}>
@@ -81,35 +100,78 @@ export const General = () => {
           >
             <FontAwesome name="lock" size={23} color="#2F80ED" />
           </View>
-          <View style={{ flex: 12 }}>
-            <Text>Password</Text>
-            <Text>Tap to change password</Text>
-          </View>
-          <View style={{ flex: 1, paddingTop: 10 }}>
-            <Feather
-              name="edit-2"
-              size={20}
-              color="#2F80ED"
-              onPress={() => setpasswordEdit(true)}
-            />
-          </View>
+          {!passwordEdit ? (
+            <>
+              <View style={{ flex: 12 }}>
+                <Text>Password</Text>
+                <Text>Tap to change password</Text>
+              </View>
+              <View style={{ flex: 1, paddingTop: 10 }}>
+                <Feather
+                  name="edit-2"
+                  size={20}
+                  color="#2F80ED"
+                  onPress={() => setpasswordEdit(true)}
+                />
+              </View>
+            </>
+          ) : (
+            <>
+              <View style={{ flex: 13 }}>
+                <TextInput
+                  style={{ backgroundColor: "white", height: 40 }}
+                  placeholder="Password"
+                  secureTextEntry={true}
+                />
+              </View>
+              <View style={{ flex: 1, paddingTop: 15 }}>
+                <Entypo
+                  name="cross"
+                  size={30}
+                  color="red"
+                  onPress={() => setpasswordEdit(false)}
+                />
+              </View>
+            </>
+          )}
         </View>
         <View style={{ flexDirection: "row", marginBottom: 10 }}>
           <View style={{ flex: 1, paddingLeft: 8, paddingTop: 9 }}>
             <Fontisto name="mobile-alt" size={20} color="#2F80ED" />
           </View>
-          <View style={{ flex: 12 }}>
-            <Text>Phone number</Text>
-            <Text>071-1234567</Text>
-          </View>
-          <View style={{ flex: 1, paddingTop: 10 }}>
-            <Feather
-              name="edit-2"
-              size={20}
-              color="#2F80ED"
-              onPress={() => setphoneEdit(true)}
-            />
-          </View>
+          {!phoneEdit ? (
+            <>
+              <View style={{ flex: 12 }}>
+                <Text>Phone number</Text>
+                <Text>071-1234567</Text>
+              </View>
+              <View style={{ flex: 1, paddingTop: 10 }}>
+                <Feather
+                  name="edit-2"
+                  size={20}
+                  color="#2F80ED"
+                  onPress={() => setphoneEdit(true)}
+                />
+              </View>
+            </>
+          ) : (
+            <>
+              <View style={{ flex: 13 }}>
+                <TextInput
+                  style={{ backgroundColor: "white", height: 40 }}
+                  placeholder="0711111111"
+                />
+              </View>
+              <View style={{ flex: 1, paddingTop: 15 }}>
+                <Entypo
+                  name="cross"
+                  size={30}
+                  color="red"
+                  onPress={() => setphoneEdit(false)}
+                />
+              </View>
+            </>
+          )}
         </View>
         <View style={{ flexDirection: "row", marginBottom: 10 }}>
           <View style={{ flex: 1, padding: 5 }}>
@@ -144,6 +206,13 @@ export const General = () => {
               onPress={() => setnameEdit(true)}
             />
           </View>
+        </View>
+        <View style={{ alignItems: "center" }}>
+          {nameEdit | emailEdit | passwordEdit | phoneEdit | paymentEdit ? (
+            <Button style={{ backgroundColor: "#2F80ED" }}>
+              <Text style={{ color: "white" }}>Submit</Text>
+            </Button>
+          ) : null}
         </View>
       </ScrollView>
     </View>
